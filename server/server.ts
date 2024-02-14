2dee808764e1f0ab41c35faa84056dfc89750983
Sharon218 <sharonshajan2017@gmail.com>
2024-02-14 21:18:32 +0530

    update

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
 
 const app = express();
-app.get("/", (req, res)=>{res.send("Hi")})
+
+// Public route
+const public_ = require("./routes/public");
+
+app.use("/", public_);
+
 app.listen(2000, () => {
   console.log("Connected to http://localhost:2000");
 });
-
