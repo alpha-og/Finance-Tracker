a3787158d268c5a7969a3393ab46431b47dcdb8f
Athul Anoop <athulanoop2020@gmail.com>
2024-02-14 21:13:27 +0530

    Restructured server and added some configuration files

--- server/server.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-app.get("/", (req, res)=>{res.send("Hi")})
-app.listen(2000, () => {
-  console.log("Connected to http://localhost:2000");
-});
-
