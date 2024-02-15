e79a655aec9c32660031a1358d6ed93adfa2fe86
Sharon218 <sharonshajan2017@gmail.com>
2024-02-15 23:49:45 +0530

    transaction-controller making

--- server/src/controllers/transaction-controller.ts
+++ server/src/controllers/transaction-controller.ts
@@ -7,19 +7,19 @@ const createTransaction = async (
   res: express.Response,
   next: express.NextFunction,
 ) => {
-  const { user_id, amount, desc } = req.body;
+  const { userId, amount, desc } = req.body;
   try {
-    const user = await User.findById(user_id, (err: Error, docs: string) => {
+    const user = await User.findById(userId, (err: Error, docs: string) => {
       if (err) {
         res.status(500).send(err.message);
       }
     });
-    const data = new transactionSchema({
-      user: user_id,
+    const newTransaction = new transactionSchema({
+      user: userId,
       amount: amount,
       description: desc,
     });
-    await data.save();
+    await newTransaction.save();
     res.status(201).send("Transaction saved");
   } catch (err) {
     if (err instanceof Error) {
@@ -35,7 +35,14 @@ const getTransactions = async (
   res: express.Response,
   next: express.NextFunction,
 ) => {
-  res.send("Hello world");
+  const { userId } = req.body;
+  try {
+    const transactions = await transactionSchema.find({ user: userId });
+    res.status(200).send(transactions);
+  } catch (err) {
+    if (err instanceof Error) res.status(500).send(err.message);
+    res.status(500).send("Server error");
+  }
 };
 
 const getTransaction = async (
@@ -43,7 +50,17 @@ const getTransaction = async (
   res: express.Response,
   next: express.NextFunction,
 ) => {
-  res.send("Hello world");
+  const { userId, transId } = req.body;
+  try {
+    const transaction = await transactionSchema.findOne({
+      user: userId,
+      _id: transId,
+    });
+    res.status(200).send(transaction);
+  } catch (err) {
+    if (err instanceof Error) res.status(500).send(err.message);
+    res.status(500).send("Server error");
+  }
 };
 
 const deleteTransaction = async (
@@ -51,7 +68,16 @@ const deleteTransaction = async (
   res: express.Response,
   next: express.NextFunction,
 ) => {
-  res.send("Hello world");
+  const { userId, transId } = req.body;
+  try {
+    const transaction = await transactionSchema.deleteOne({
+      user: userId,
+      _id: transId,
+    });
+  } catch (err) {
+    if (err instanceof Error) res.status(500).send(err.message);
+    res.status(500).send("Server error");
+  }
 };
 
 const updateTransaction = async (
@@ -59,7 +85,17 @@ const updateTransaction = async (
   res: express.Response,
   next: express.NextFunction,
 ) => {
-  res.send("Hello world");
+  const { userId, amount, desc, transId } = req.body;
+  try {
+    const transaction = await transactionSchema.updateOne(
+      { _id: transId, user: userId },
+      { amount: amount, description: desc },
+    );
+    res.status(200).send("Updated successfully");
+  } catch (err) {
+    if (err instanceof Error) res.status(500).send(err.message);
+    res.status(500).send("Server error");
+  }
 };
 
 export {
