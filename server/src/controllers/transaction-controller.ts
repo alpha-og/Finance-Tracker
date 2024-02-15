ac52c8f6ef7e7aae3cb9bfa482ce4e4f49ef4553
Sharon218 <sharonshajan2017@gmail.com>
2024-02-15 22:16:14 +0530

    update

--- server/src/controllers/transaction-controller.ts
+++ server/src/controllers/transaction-controller.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import transactionSchema from "../models/transaction";
+import transactionSchema from "../models/transaction-model";
 import User from "../models/user-model";
 
 const createTransaction = async (
