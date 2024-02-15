df93514ca571d97512b9d9e42e9e744777ebe2e4
Roshin R <roshinr2023@gmail.com>
2024-02-15 21:31:49 +0530

    updated user model

--- server/src/models/user-model.ts
+++ server/src/models/user-model.ts
@@ -3,11 +3,13 @@ import mongoose, { Document } from "mongoose";
 export interface UserModel extends Document {
   username: string;
   email: string;
+  password:string;
 }
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true ,unique:true},
+  password:{type:String,required:true}
 });
 
 export default mongoose.model<UserModel>("User", userSchema);
