841ebf026e2d6f826fdbd8f8947f56e0aa839832
Athul Anoop <athulanoop2020@gmail.com>
2024-02-18 22:42:56 +0530

    Update Input.tsx

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { IconType } from "react-icons";
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   className?: string;
+  icon?: IconType;
 };
-const Input = (props: InputProps) => {
+const Input = ({ icon, ...props }: InputProps) => {
   !props.className &&
     (props = {
       className:
-        "w-full h-12 p-4 bg-neutral-800 outline-none rounded-lg focus:ring-1 focus:ring-white ease-in-out duration-300",
+        "w-full h-12 px-4 py-2 bg-neutral-800 outline-none rounded-lg focus:ring-1 focus:ring-white ease-in-out duration-300",
       ...props,
     });
   return <input {...props} />;
