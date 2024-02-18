826dd9a6f112ab8a35561fb0f031b99b24a13e89
Athul Anoop <athulanoop2020@gmail.com>
2024-02-18 23:14:03 +0530

    added margin to center (visually) the signup card
    this is a temporary fix and a more robust fix needs to be issued in the near future

--- client/src/app/user/(auth)/signup/page.tsx
+++ client/src/app/user/(auth)/signup/page.tsx
@@ -4,16 +4,11 @@ import Link from "next/link";
 import { FaUserCircle } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Button, Input } from "@/components/components";
-import { useState } from "react";
 
 export default function Signup() {
-  const [errorToastVisibility, setErrorToastVisibility] = useState<
-    "hidden" | "visible"
-  >("hidden");
-  console.log(errorToastVisibility);
   return (
     <div
-      className="w-80 sm:w-[28rem] min-h-3/4 p-8 sm:p-12 flex flex-col justify-center items-center gap-8 bg-neutral-950 rounded-lg shadow-lg shadow-black ease-in-out duration-300"
+      className="w-80 sm:w-[28rem] min-h-3/4 p-8 mt-16 sm:p-12 flex flex-col justify-center items-center gap-8 bg-neutral-950 rounded-lg shadow-lg shadow-black ease-in-out duration-300"
       onSubmit={(e) => e.preventDefault()}
     >
       <FaUserCircle className="w-14 sm:w-16 h-14 sm:h-16 flex-shrink-0 flex-grow-0 ease-in-out duration-300" />
