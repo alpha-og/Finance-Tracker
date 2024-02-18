aca0b57318cba413f16f248cbf38b8399729d746
Sharon218 <sharonshajan2017@gmail.com>
2024-02-18 18:43:33 +0530

    adding transaction update client

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,11 @@
 import Image from "next/image";
+import add_transaction from "@/components/transaction-updater";
+
+add_transaction({
+  user: "65cf86b3f1c0f913afddfc3d",
+  amount: 10000,
+  description: "Dummy UI",
+});
 
 export default function Home() {
   return <h1>Finance Tracker</h1>;
