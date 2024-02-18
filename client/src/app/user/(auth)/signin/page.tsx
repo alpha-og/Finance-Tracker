c5a1cf6d6752d4d47329e7411546d84a5a36f62a
Athul Anoop <athulanoop2020@gmail.com>
2024-02-18 22:42:24 +0530

    Authentication pages
    Created a sign up page and moved the auth related routes into the auth route group

--- client/src/app/user/(auth)/signin/page.tsx
+++ client/src/app/user/(auth)/signin/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { FaUserCircle } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
-import { Input } from "@/components/components";
+import { Button, Input } from "@/components/components";
 
 export default function Signin() {
   return (
@@ -13,19 +13,11 @@ export default function Signin() {
       <form className="w-full flex flex-col justify-center items-center gap-4 text-sm sm:text-md ease-in-out duration-300">
         <Input type="email" placeholder="Your email address..." />
         <Input type="password" placeholder="Type in your password..." />
-        <button
-          type="submit"
-          className="w-full max-h-12 p-2 text-lg text-neutral-950 font-bold bg-white rounded-lg focus:ring-white hover:bg-blue-500 hover:text-white duration-300"
-        >
-          Sign in
-        </button>
-        <button
-          type="submit"
-          className="w-full max-h-12 p-2 text-lg text-neutral-950 font-bold bg-white rounded-lg flex justify-center items-center gap-2 hover:bg-blue-500 hover:text-white focus:ring-white duration-300 "
-        >
+        <Button type="submit">Sign in</Button>
+        <Button type="submit">
           <FcGoogle size={28} />
           <p>Sign in with Google</p>
-        </button>
+        </Button>
       </form>
       <p className="w-full text-center text-neutral-500 ease-in-out duration-300">
         Don't have an account?
