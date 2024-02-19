3332bb676da289bfa2ce9e59abfbb2b88b927d31
Athul Anoop <athulanoop2020@gmail.com>
2024-02-20 00:21:35 +0530

    Created dashboard page

---
+++ client/src/app/user/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { TransactionsList } from "@/components/components";
+
+const Dashboard = () => {
+  return (
+    <div className="w-full h-full flex justify-evenly">
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
