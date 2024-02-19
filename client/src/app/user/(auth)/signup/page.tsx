7acda40978c880a9c7d4ebee7e2f11860a0817d4
Athul Anoop <athulanoop2020@gmail.com>
2024-02-20 00:21:01 +0530

    Update page.tsx

--- client/src/app/user/(auth)/signup/page.tsx
+++ client/src/app/user/(auth)/signup/page.tsx
@@ -8,7 +8,7 @@ import { Button, Input } from "@/components/components";
 export default function Signup() {
   return (
     <div
-      className="w-80 sm:w-[28rem] min-h-3/4 p-8 mt-16 sm:p-12 flex flex-col justify-center items-center gap-8 bg-neutral-950 rounded-lg shadow-lg shadow-black ease-in-out duration-300"
+      className="w-80 sm:w-[28rem] min-h-3/4 p-8 sm:p-12 flex flex-col justify-center items-center gap-8 bg-neutral-950 rounded-lg shadow-lg shadow-black ease-in-out duration-300"
       onSubmit={(e) => e.preventDefault()}
     >
       <FaUserCircle className="w-14 sm:w-16 h-14 sm:h-16 flex-shrink-0 flex-grow-0 ease-in-out duration-300" />
