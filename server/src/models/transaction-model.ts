3f63c5005e864dd22c7bd9da89a196eef6230f5c
Athul Anoop <athulanoop2020@gmail.com>
2024-02-20 10:52:49 +0530

    Update transaction-model.ts
    Added title, category, location, time, date and timestamping to transaction model

--- server/src/models/transaction-model.ts
+++ server/src/models/transaction-model.ts
@@ -1,16 +1,54 @@
 import mongoose, { Document } from "mongoose";
 
+export type T_transactionCategory =
+  | "Food"
+  | "Healthcare"
+  | "Leisure"
+  | "Groceries"
+  | "Transportation"
+  | "Utilities"
+  | "Shopping"
+  | "Education"
+  | "Travel"
+  | "Miscellaneous";
+
+const transactionCategories: T_transactionCategory[] = [
+  "Food",
+  "Healthcare",
+  "Leisure",
+  "Groceries",
+  "Transportation",
+  "Utilities",
+  "Shopping",
+  "Education",
+  "Travel",
+  "Miscellaneous",
+];
+
 export interface TransactionModel extends Document {
   user: string;
-  amount: number;
+  title: string;
   description: string;
+  amount: number;
+  location: string;
+  category: T_transactionCategory;
+  date: Date;
+  time: Date;
 }
 
-const transactionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.ObjectId, ref: "User" },
-  amount: { type: Number, required: true },
-  description: { type: String, required: true },
-});
+const transactionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.ObjectId, ref: "User" },
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    amount: { type: Number, required: true },
+    location: { type: String, required: false },
+    category: { enum: transactionCategories, required: true },
+    date: { type: Date },
+    time: { type: Date },
+  },
+  { timestamps: true },
+);
 
 export default mongoose.model<TransactionModel>(
   "Transaction",
